e3eee1013232c4df43817361847a67becb5bb40d
RedTheDraken <kangpablo@gmail.com>
2014-06-01 22:51:41 -0700

    Functionality added

--- Project 2/Bridge.cpp
+++ Project 2/Bridge.cpp
@@ -1,73 +1,142 @@
-#include "Bridge.h"
+#include <string>
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <pthread.h>
 
 
-// GLOBAL BRIDGE OBJECT:
-Bridge bridge;
+using namespace std;
 
 
-Bridge::Bridge()
+#define DEBUG		0
+#define LEFT_SIDE	0
+#define RIGHT_SIDE	1
+
+
+// VEHICLE FUNCTION DECLARATIONS:
+void* OneVehicle(void* params);
+int ArriveBridge(int id, int direc);
+void CrossBridge(int id, int direc);
+void ExitBridge(int id, int direc, int index);
+
+
+// FUNC_PARAMS STRUCT ////////////////////////////////////////////////////////////////////////////////////////////
+struct func_params
 {
-	string fileName = "";
-	
-	cout << "Enter input file name: ";
-	getline(cin, fileName);
-	
-	fstream inFile(fileName.c_str());
-	
-	if (!inFile){
-		cout << "ERROR >> File not found or could not be opened.\n";
+	int carID;			// ID of car.
+	int direction;		// Intended destination (LEFT_SIDE / RIGHT_SIDE) of car.
+
+	// Struct Constructor
+	func_params(int id, int dir)
+	{
+		carID = id;
+		direction = dir;
 	}
-	else {
-		inFile >> carCount;
+};
 
-		// Fill both Queues with threads of OneVehicle, passed car information from input file.
-		for (int i = 0; i < carCount; i++){
-			int carID;
-			int carDir;
 
-			inFile >> carID;
-			inFile >> carDir;
+// VEHICLE STRUCT //////////////////////////////////////////////////////////////////////////////////////////////
+struct vehicle
+{
+	int carID;
+	int carDir;
 
-			pthread_t carThread;
-			func_params* carData = new func_params(carID, carDir);
+	vehicle(int id, int dir)
+	{
+		carID = id;
+		carDir = dir;
+	}
+};
 
-			pthread_create(&carThread, NULL, OneVehicle, (void*)carData);
 
-			if (carDir == LEFT_SIDE) {
-				travelingLeftQueue.push(carThread);
-			}
-			else if (carDir == RIGHT_SIDE) {
-				travelingRightQueue.push(carThread);
-			}
-			else {
-				cout << "ERROR >> Invalid direction of travel across bridge.\n";
-			}
+// BRIDGE CLASS ////////////////////////////////////////////////////////////////////////////////////////////////
+class Bridge
+{
+public:
+	int trafficDirection;						// Current direction of traffic flow.
+	int carCount;								// Number of cars in the input file.
+	int traveler;								// Dynamic array of current travelers across bridge.
+
+	int CAR_CAPACITY = 3;						// Max cars the bridge can support at any given time.
+
+	queue<vehicle> left_ID_queue;				// Queue of vehicle IDs & directions traveling from right to left.
+	queue<vehicle> right_ID_queue;				// Queue of vehicle IDs & directions traveling from left to right.
+	vector<pthread_t> car_threads;				// Vector of all running car threads.
+	vector<pthread_t>::iterator threadIter;
+
+	// Constructor
+	Bridge() 
+	{
+		string fileName = "";
+
+		cout << "Enter input file name: ";
+		getline(cin, fileName);
+
+		fstream inFile(fileName.c_str());
+
+		if (!inFile){
+			cout << "ERROR >> File not found or could not be opened.\n";
 		}
-		inFile.close();
+		else {
+			inFile >> carCount;
 
-		// Set current traveler on bridge to nobody.
-		traveler = 0;
+			car_threads = vector<pthread_t>(carCount);
+
+			// Fill both Queues with car info from input file and Vector with car threads.
+			for (int i = 0; i < carCount; i++){
+				int carID;
+				int carDir;
+
+				inFile >> carID;
+				inFile >> carDir;
+
+				vehicle car(carID, carDir);
+
+				if (carDir == LEFT_SIDE) 
+					left_ID_queue.push(car);
+				else if (carDir == RIGHT_SIDE) 
+					right_ID_queue.push(car);
+				else 
+					cout << "ERROR >> Invalid direction of travel across bridge.\n";
+
+				pthread_t carThread;
+				func_params* carData = new func_params(carID, carDir);
+				pthread_create(&carThread, NULL, OneVehicle, (void*)carData);
+
+				threadIter = car_threads.begin();
+				car_threads.insert(threadIter + (carID-1), carThread);
+			}
+			inFile.close();
+
+			// Set current traveler on bridge to nobody.
+			traveler = 0;
+		}
 	}
-}
 
 
-Bridge::~Bridge()
-{
-
-}
-
+	// Deconstructor
+	~Bridge() {}
+};
 
 
+// GLOBAL DATA DECLARATIONS:
+Bridge bridge;			// Object of Bridge
+pthread_mutex_t LOCK;	// Mutual exclusion lock
+int currentDirection;
+int carCounter;
 
-// VEHICLE FUNCTIONS ///////////////////////////////////////////////////////////////////////////////
 
+// VEHICLE FUNCTIONS ////////////////////////////////////////////////////////////////////////////////////////////////////
 // OneVehicle() - Represents one vehicle, and executes the three functions below it.
 void* OneVehicle(void* params){
-	func_params* p = (func_params*) params;
+	func_params* p = (func_params*)params;
 	int travelerIndex;
 	travelerIndex = ArriveBridge(p->carID, p->direction);
 	CrossBridge(p->carID, p->direction);
 	ExitBridge(p->carID, p->direction, travelerIndex);
+	
+	return 0;
 }
 
 // ArriveBridge() - Executed by OneVehicle(). Does not return until it is safe for this "vehicle" to cross the bridge.
@@ -80,7 +149,7 @@ int ArriveBridge(int id, int direc)
 
 	while (waiting){
 		cout << "Vehicle " << id << " is waiting to cross the bridge in direction " << direc << endl;
-		
+
 		if (bridge.traveler == id) {
 			waiting = false;
 		}
@@ -91,32 +160,50 @@ int ArriveBridge(int id, int direc)
 // CrossBridge() - Executed by OneVehicle(). Used for debugging/checking if vehicles correctly cross bridge.
 void CrossBridge(int id, int direc)
 {
-	if (DEBUG) 
+	pthread_mutex_trylock(&LOCK);	// lock the carCounter
+	carCounter++;
+	pthread_mutex_unlock(&LOCK);	// unclock the carCounter
+
+	if (DEBUG)
 		cout << "DEBUG >> Vehicle " << id << " is crossing the bridge in direction " << direc << endl;
 }
 
 // ExitBridge() - Called when vechicle crosses bridge and performs operations to maintain flow of traffic.
 void ExitBridge(int id, int direc, int index)
 {
+	pthread_mutex_trylock(&LOCK);	// lock the current traveler & carCounter
 	bridge.traveler = 0;
+	carCounter--;
+	pthread_mutex_unlock(&LOCK);	// unclock the current traveler & carCounter
 }
 
 
-// MAIN FUNCTION OF BRIDGE CONTROL ///////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// MAIN FUNCTION OF BRIDGE CONTROL ////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, const char * argv[])
 {
 	bridge = Bridge();
 	
-	int currentDirection = 0;
+	currentDirection = LEFT_SIDE;
+	carCounter = 0;
 
-	while (!bridge.travelingLeftQueue.empty() || !bridge.travelingRightQueue.empty()) {
-		if (currentDirection == 0 && bridge.travelingLeftQueue.empty())
-			currentDirection = 1;
-		else if (currentDirection == 1 && bridge.travelingRightQueue.empty())
-			currentDirection = 0;
+	while (!bridge.left_ID_queue.empty() || !bridge.right_ID_queue.empty()) {
+		// Check if only one side has exhausted all vehicles or capacity reached.
+		if (currentDirection == LEFT_SIDE && bridge.left_ID_queue.empty() || carCounter == bridge.CAR_CAPACITY)
+			currentDirection = RIGHT_SIDE;
+		else if (currentDirection == RIGHT_SIDE && bridge.right_ID_queue.empty() || carCounter == bridge.CAR_CAPACITY)
+			currentDirection = LEFT_SIDE;
 
-		
+		// If bridge has room in current direction.
+		if (carCounter < bridge.CAR_CAPACITY) {
+			// Get carID of next available traveler.
+			if (currentDirection == LEFT_SIDE && bridge.left_ID_queue.front().carDir == LEFT_SIDE)
+				bridge.traveler = bridge.left_ID_queue.front().carID;
+
+			else if (currentDirection == RIGHT_SIDE && bridge.right_ID_queue.front().carDir == RIGHT_SIDE)
+				bridge.traveler = bridge.right_ID_queue.front().carID;
+		}
 	}
-
 	return 0;
 }
--- Project 2/Bridge.h
+++ Project 2/Bridge.h
@@ -14,7 +14,19 @@ using namespace std;
 #define RIGHT_SIDE	1
 
 
-// Parameters to the OneVehicle function
+// GLOBAL DATA DECLARATIONS:
+Bridge bridge;			// Object of Bridge
+pthread_mutex_t LOCK;	// Mutual exclusion lock
+
+
+// VEHICLE FUNCTION DECLARATIONS:
+void* OneVehicle(void* params);
+int ArriveBridge(int id, int direc);
+void CrossBridge(int id, int direc);
+void ExitBridge(int id, int direc, int index);
+
+
+// FUNC_PARAMS STRUCT ////////////////////////////////////////////////////////////////////////////////////////////
 struct func_params
 {
 	int carID;			// ID of car.
@@ -29,6 +41,24 @@ struct func_params
 };
 
 
+// VEHICLE STRUCT //////////////////////////////////////////////////////////////////////////////////////////////
+struct vehicle
+{
+	pthread_t carThread;
+	int carID;
+	int carDir;
+
+	vehicle(int id, int dir)
+	{
+		carID = id;
+		carDir = dir;
+		func_params* carData = new func_params(carID, carDir);
+		pthread_create(&carThread, NULL, OneVehicle, (void*)carData);
+	}
+};
+
+
+// BRIDGE CLASS ////////////////////////////////////////////////////////////////////////////////////////////////
 class Bridge
 {
 public:
@@ -38,8 +68,8 @@ public:
 
 	int CAR_CAPACITY = 3;						// Max cars the bridge can support at any given time.
 
-	queue<pthread_t> travelingLeftQueue;		// Queue of vehicle threads traveling from right to left.
-	queue<pthread_t> travelingRightQueue;		// Queue of vehicle threads traveling from left to right.
+	queue<vehicle> travelingLeftQueue;		// Queue of vehicle threads traveling from right to left.
+	queue<vehicle> travelingRightQueue;		// Queue of vehicle threads traveling from left to right.
 
 
 	// Constructor
@@ -48,3 +78,48 @@ public:
 	~Bridge();
 };
 
+
+
+// VEHICLE FUNCTIONS ///////////////////////////////////////////////////////////////////////////////
+
+// OneVehicle() - Represents one vehicle, and executes the three functions below it.
+void* OneVehicle(void* params){
+	func_params* p = (func_params*)params;
+	int travelerIndex;
+	travelerIndex = ArriveBridge(p->carID, p->direction);
+	CrossBridge(p->carID, p->direction);
+	ExitBridge(p->carID, p->direction, travelerIndex);
+}
+
+// ArriveBridge() - Executed by OneVehicle(). Does not return until it is safe for this "vehicle" to cross the bridge.
+int ArriveBridge(int id, int direc)
+{
+	bool waiting = true;
+	int index = 0;
+
+	cout << "Vehicle " << id << " has arrived at the bridge, traveling in direction " << direc << endl;
+
+	while (waiting){
+		cout << "Vehicle " << id << " is waiting to cross the bridge in direction " << direc << endl;
+
+		if (bridge.traveler == id) {
+			waiting = false;
+		}
+	}
+	return index;
+}
+
+// CrossBridge() - Executed by OneVehicle(). Used for debugging/checking if vehicles correctly cross bridge.
+void CrossBridge(int id, int direc)
+{
+	if (DEBUG)
+		cout << "DEBUG >> Vehicle " << id << " is crossing the bridge in direction " << direc << endl;
+}
+
+// ExitBridge() - Called when vechicle crosses bridge and performs operations to maintain flow of traffic.
+void ExitBridge(int id, int direc, int index)
+{
+	pthread_mutex_trylock(&LOCK);	// lock the current traveler
+	bridge.traveler = 0;
+	pthread_mutex_unlock(&LOCK);	// unclock the current traveler
+}
